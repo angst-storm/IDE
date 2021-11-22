51a45df78a28737ce98bd875b9e43e0df99cc966
angst-storm <Sergey.Kiprin@urfu.me>
2021-11-22 22:55:50 +0500

    7

---
+++ filter_with_filename.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+
+
+def median_gray(arr, step):
+    return int(np.mean(arr) // step) * step
+
+
+def gray_pixelation(image, size, gradations):
+    step = int(255 // (gradations - 1))
+    arr = np.array(image)
+    for row in range(0, len(arr), size):
+        for column in range(0, len(arr[1]), size):
+            arr[row:row + size, column:column + size] = \
+                median_gray(arr[row:row + size, column:column + size], step)
+    return Image.fromarray(arr)
+
+
+if __name__ == "__main__":
+    gray_pixelation(Image.open("galochka.jpg"), 10, 5).save("result-2.jpg")
