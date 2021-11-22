4c901a06a479b311b59960447273d33a79d6cded
angst-storm <Sergey.Kiprin@urfu.me>
2021-11-22 22:36:53 +0500

    4

--- filter.py
+++ filter.py
@@ -1,4 +1,4 @@
-import sys
+# import sys
 from PIL import Image
 import numpy as np
 
@@ -10,12 +10,19 @@ def median_gray(arr, step):
 def gray_pixelation(image, size, gradations):
     step = int(255 // (gradations - 1))
     arr = np.array(image)
-    for row in range(0, len(arr) - size + 1, size):
-        for column in range(0, len(arr[1]) - size + 1, size):
+    for row in range(0, len(arr), size):
+        for column in range(0, len(arr[1]), size):
             arr[row:row + size, column:column + size] = \
                 median_gray(arr[row:row + size, column:column + size], step)
     return Image.fromarray(arr)
 
 
 if __name__ == "__main__":
-    gray_pixelation(Image.open(sys.argv[1]), 10, 5).save(sys.argv[2])
+    # gray_pixelation(Image.open(sys.argv[1]), 10, 5).save(sys.argv[2])
+    gray_pixelation(
+        Image.open(input("Имя исходного изображения: ")),
+        int(input("Размер пикселя: ")),
+        int(input("Число градаций серого: "))
+    ).save(
+        input("Имя результата: ")
+    )
